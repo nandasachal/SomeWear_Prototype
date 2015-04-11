b018be257e92a981697bab9c8aa760af3913ce13
Linda <he.linda.j@gmail.com>
2015-04-11 12:06:24 -0700

    testing github stuff

--- Redtooth (Device)/src/main.js
+++ Redtooth (Device)/src/main.js
@@ -1,1 +1,14 @@
 //@program
+
+var whiteSkin = new Skin( { fill:"white" } );
+var labelStyle = new Style( { font: "bold 40px", color:"black" } );
+var mainContainer = new Container({
+  left:0, right:0, top:0, bottom:0,
+  skin: whiteSkin,
+  contents:[
+    new Label({left:0, right:0, height: 40, string: "Hi RedTooth!", style: labelStyle})
+  ]
+});
+
+
+application.add( mainContainer );
