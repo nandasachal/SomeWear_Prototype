42dbb1bc86d895eb740053e5d395361587c721da
DarryQueen <slimfrog@gmail.com>
2015-04-11 16:31:27 -0700

    Added add clothing modal.

---
+++ RedTooth (Mobile)/src/addClothingModal.js
@@ -0,0 +1,116 @@
+//@module
+
+var THEME = require('themes/sample/theme');
+var CONTROL = require('mobile/control');
+var KEYBOARD = require('mobile/keyboard');
+
+var nameInputSkin = new Skin({ borders: { left:2, right:2, top:2, bottom:2 }, stroke: 'gray',});
+var fieldStyle = new Style({ color: 'black', font: 'bold 24px', horizontal: 'left', vertical: 'middle', left: 5, right: 5, top: 0, bottom: 0, });
+var fieldHintStyle = new Style({ color: '#aaa', font: '20px', horizontal: 'left', vertical: 'middle', left: 5, right: 5, top: 0, bottom: 0, });
+var whiteSkin = new Skin({fill:"transparent"});
+
+var title = ''
+
+function getClothingInfo() {
+  return {
+    title: title
+  }
+}
+
+var TitleField = Container.template(function($) { return { 
+  width: 250, height: 36, skin: nameInputSkin, contents: [
+    Scroller($, { 
+      left: 4, right: 4, top: 4, bottom: 4, active: true, 
+      behavior: Object.create(CONTROL.FieldScrollerBehavior.prototype), clip: true, contents: [
+        Label($, { 
+          left: 0, top: 0, bottom: 0, skin: THEME.fieldLabelSkin, style: fieldStyle, anchor: 'TITLE',
+          editable: true, string: $.name,
+            behavior: Object.create( CONTROL.FieldLabelBehavior.prototype, {
+                onEdited: { value: function(label){
+                    var data = this.data;
+                    data.name = label.string;
+                    label.container.titleHint.visible = ( data.name.length == 0 ); 
+                }}
+            }),
+         }),
+         Label($, {
+                left:4, right:4, top:4, bottom:4, style:fieldHintStyle, string:"Clothing Title", name:"titleHint"
+         })
+      ]
+    })
+  ]
+}});
+
+var CategoriesField = Container.template(function($) { return { 
+  width: 250, height: 36, skin: nameInputSkin, contents: [
+    Scroller($, { 
+      left: 4, right: 4, top: 4, bottom: 4, active: true, 
+      behavior: Object.create(CONTROL.FieldScrollerBehavior.prototype), clip: true, contents: [
+        Label($, { 
+          left: 0, top: 0, bottom: 0, skin: THEME.fieldLabelSkin, style: fieldStyle, anchor: 'CATEGORIES',
+          editable: true, string: $.name,
+            behavior: Object.create( CONTROL.FieldLabelBehavior.prototype, {
+                onEdited: { value: function(label){
+                    var data = this.data;
+                    data.name = label.string;
+                    label.container.categoriesHint.visible = ( data.name.length == 0 ); 
+                }}
+            }),
+         }),
+         Label($, {
+                left:4, right:4, top:4, bottom:4, style:fieldHintStyle, string:"Clothing Categories", name:"categoriesHint"
+         })
+      ]
+    })
+  ]
+}});
+
+var labelStyle = new Style( { font: "bold 30px", color:"black" } );
+var OkayButton = BUTTONS.Button.template(function($) { return {
+    left: 0, right: 0, height: 50, width: 100,
+    contents: [
+        new Label({left:0, right:0, height:40, string: "Okay", style: labelStyle})
+    ],
+    behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+        onTap: { value: function(content) {
+            application.remove(modal);
+        }},
+    })
+}});
+var CancelButton = BUTTONS.Button.template(function($) { return {
+    left: 0, right: 0, height: 50, width: 100,
+    contents: [
+        new Label({left:0, right:0, height:40, string: "Cancel", style: labelStyle})
+    ],
+    behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+        onTap: { value: function(content) {
+            application.remove(modal);
+        }},
+    })
+}});
+
+var whiteSkin = new Skin( { fill:"green" } );
+var labelStyle = new Style( { font: "bold 30px", color:"black" } );
+
+var buttons = new Line({
+    contents: [
+      new CancelButton(),
+      new OkayButton(),
+    ]
+});
+
+var modal = new Column({
+    left: 20, right: 20, top: 20, bottom: 20, skin: whiteSkin,
+    contents: [
+        new Label({string:"Add Clothing", style: labelStyle, top: 20}),
+        new Container({ height: 50 }),
+        new TitleField({ name: '' }),
+        new Container({ height: 10 }),
+        new CategoriesField({ name: '' }),
+        new Container({ height: 30 }),
+        buttons,
+    ]
+});
+
+exports.modal = modal;
+exports.getClothingInfo = getClothingInfo;
--- RedTooth (Mobile)/src/main.js
+++ RedTooth (Mobile)/src/main.js
@@ -3,8 +3,12 @@
 var THEME = require('themes/sample/theme');
 var SCROLLER = require('mobile/scroller');
 var SCREEN = require('mobile/screen');
+var CONTROL = require('mobile/control');
+var KEYBOARD = require('mobile/keyboard');
+var BUTTONS = require('controls/buttons');
 
 var scrollingExample = require("scrollingexample.js");
+var addClothingModal = require("addCLothingModal.js");
 
 application.behavior = Object.create(Object.prototype, {
 	onLaunch: { value: function(application) {
