a422f8b0292222856a3899618d307495e63ec4dd
Linda <linda.he@berkeley.edu>
2015-04-11 18:04:22 -0700

    working on clothing screen

---
+++ RedTooth (Mobile)/src/clothing.js
@@ -0,0 +1,182 @@
+//@program
+
+var THEME = require('themes/sample/theme');
+var SCROLLER = require('mobile/scroller');
+var SCREEN = require('mobile/screen');
+//var thumbnailWidth = Math.floor( application.width / 2 ) - 10;
+//var thumbnailHeight = thumbnailWidth;
+
+
+/*var ClothingContainer = Container.template(function($) { return {
+	left:0, right:0, top:0, bottom:0,
+	contents: [
+	   		 //Note that the scroller is declared as having only an empty
+	   		 //Column and a scrollbar.  All the entries will be added 
+	   		 // programmatically. 
+	   		SCROLLER.VerticalScroller($, { 
+	   			contents: [
+              			Column($, { left: 0, right: 0, top: 0, name: 'menu', }),
+              			SCROLLER.VerticalScrollbar($, { }),
+              			]
+	   		})
+	   		]
+	}});
+	
+
+var clothingList = [
+    	{name: 'blue shirt', idNum: 1},
+    	{name: 'gray shirt', idNum: 2},
+    	{name: 'black shirt1', idNum: 3},
+    	{name: 'black shirt2', idNum: 4},
+    	{name: 'black shirt3', idNum: 5},
+    	{name: 'black shirt4', idNum: 6},
+    	{name: 'black shirt5', idNum: 7},
+    	];
+    	
+application.add(new ClothingContainer());
+trace("application.width = " + application.width);*/
+    
+
+
+//@program
+
+// testing scroller
+
+var THEME = require('themes/sample/theme');
+var SCROLLER = require('mobile/scroller');
+var SCREEN = require('mobile/screen');
+
+/* ASSETS */
+var blackSkin = new Skin({ fill: 'black',});
+var whiteSkin = new Skin({ fill: 'white',});
+var yellowSkin	= new Skin({ fill: 'yellow'});
+var blueSkin = new Skin({fill: 'blue'})
+var separatorSkin = new Skin({ fill: 'silver',});
+
+/* STYLES */
+var productNameStyle = new Style({  font: 'bold 22px', horizontal: 'left', vertical: 'middle', lines: 1, });
+var productDescriptionStyle = new Style({  font: '18px', horizontal: 'left', vertical: 'middle', left: 1, color: 'white' });
+
+/* STATIC */
+/* A simple array of objects. Each will be used as a single
+ * entry in our scrollable list. */
+/*var menuItems = [
+    	{title: 'Pentium', button: 'P5'},
+    	{title: 'Pentium MMX', button: 'Tillamook'},
+    	{title: 'Pentium Pro', button: 'P6'},
+    	{title: 'Pentium II', button: 'Klamath'},
+    	{title: 'Pentium III', button: 'Coppermine'},
+    	{title: 'Pentium IV', button: 'Prescott'},
+    	{title: 'Pentium M', button: 'Dothan'},
+    	{title: 'Core Duo', button: 'Yonah'},
+    	{title: 'Core 2 Duo', button: 'Penryn'},
+    	{title: 'Core i7', button: 'Sandy Bridge'}
+    	];*/
+    	
+    	
+    	
+var clothingList = [
+    	{name: 'blue shirt', idNum: 1, photo:"../assets/shirt.png", toggleOn: false},
+    	{name: 'gray shirt', idNum: 2, photo:"../assets/shirt.png", toggleOn: false},
+    	{name: 'black shirt1', idNum: 3, photo: "../assets/shirt.png", toggleOn: false},
+    	{name: 'black shirt2', idNum: 4, photo: "../assets/shirt.png", toggleOn: false},
+    	{name: 'black shirt3', idNum: 5, photo: "../assets/shirt.png", toggleOn: false},
+    	{name: 'black shirt4', idNum: 6, photo: "../assets/shirt.png", toggleOn: false},
+    	{name: 'black shirt5', idNum: 7, photo: "../assets/shirt.png", toggleOn: false},
+];
+
+
+var imageDimension = 120;
+
+/* This is a template that will be used to for each entry populating the list. 
+ * Note that it is anticipating an object each time in is instanciated */
+var ClothingPopulate = Container.template(function($) { return { left: 0, right: 0, top: 0, active: true, //skin: whiteSkin, //skin: THEME.lineSkin, 
+    behavior: Object.create(Behavior.prototype, {
+    	/* Gives the user some visual feedback on which entry they have tapped.
+    	 * note that the skin is reverted to white in onTouchEnded() */    	 
+    	onTouchBegan: { value: function(container, id, x,  y, ticks) {
+    		//container.skin = yellowSkin;
+    	}},
+    	/* Traces out the value of the first Label's string. The
+    	 * silly string of "first" in the trace can be thought of as
+    	 * container.Column.Container.Label.string.  This pattern can
+    	 * be seen reading down the contents of this object below */
+    	onTouchEnded: { value: function(container, id, x,  y, ticks) {	
+			if (!$.toggleOn) {
+				container.first.skin = yellowSkin;
+				$.toggleOn = true;
+				trace(container.first.last.string+"\n");
+			} else if ($.toggleOn) {
+				container.first.skin = whiteSkin;
+				$.toggleOn = false;
+				trace(container.first.last.string+"\n");
+			}
+		}}
+    }),
+	contents: [
+     	//Column($, { left: 0, right: 0, contents: [
+     		Container($, { left: 0, right: 0, height: 100, width: 100, skin: whiteSkin,
+     			contents: [
+     			           /* This label expects that the object passed to ProcessorLine() 
+     			            * includes a value for title.  Note that this Label is not marked
+     			            * as active. Touches registered here will bubble back up to the
+     			            * nested objects until it hits one which is active. */
+     			           /* This label is expecting a value for button.  Note that this Label
+     			            * is marked active.  Touches registered here will be handeled here */
+     			           //Label($, { right: 10, style: productDescriptionStyle, skin: blueSkin, active: true, string: $.button,
+     			           //    behavior: Object.create(Behavior.prototype, {
+     			           		    	/* When this label is touched, simply trace out its string.
+     			           		    	 * Note that no chain of "first" is needed here because the
+     			           		    	 * touch happened in the object that contains the property
+     			           		    	 * we want to trace */
+     			           	//	    	onTouchEnded: { value: function(label, id, x,  y, ticks) {	
+							//				trace(label.string+"\n");
+							//			}}
+							//	})
+     			           //}),
+     			           new Picture( {left:0, right:0, top:0, width: 100, height: 100, name: 'picture', url: $.photo,}),
+     			           Label($, { style: productNameStyle, string: $.name,}),
+ 			           ], 
+	           }),
+     		//Line($, { left: 0, right: 0, top: 50, height: 1, skin: separatorSkin, }),
+     	//], }),
+     ], 
+ }});
+
+/* This is template for a container which takes up the
+ * whole screen.  It contains only a single object,
+ * the SCROLLER.VerticalScroller.  Although we are not
+ * referencing any values from an object passed on creation,
+ * an object is still required as the SCROLLER uses it internally. */
+var ScreenContainer = Container.template(function($) { return {
+	left:0, right:0, top:40, bottom:0,
+	contents: [
+	   		/* Note that the scroller is declared as having only an empty
+	   		 * Column and a scrollbar.  All the entries will be added 
+	   		 * programmatically. */ 
+	   		SCROLLER.VerticalScroller($, { 
+	   			contents: [
+              			Column($, { left: 0, right: 0, top: 0, name: 'menu', }),
+              			SCROLLER.VerticalScrollbar($, { }),
+              			]
+	   		})
+	   		]
+	}});
+
+var data = new Object();
+var screen = new ScreenContainer(data);
+
+/* This simple function exists so we can call "forEach" on
+ * our array of list entries (menuItems).  It adds a new 
+ * ProcessorLine() object to the Column named "menu" in the
+ * screen object's SCROLLER */
+function ListBuilder(element, index, array) {
+	screen.first.menu.add(new ClothingPopulate(element));
+}
+
+exports.ListBuilder = ListBuilder;
+exports.clothingList = clothingList;
+exports.screen = screen;
+
+
+
