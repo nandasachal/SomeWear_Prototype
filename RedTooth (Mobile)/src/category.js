2dae53a77e8c412e27eda0647f30ae65ea39b92b
Ryan Higgins <ryanhigs@gmail.com>
2015-04-12 01:58:59 -0700

    new category object

---
+++ RedTooth (Mobile)/src/category.js
@@ -0,0 +1,30 @@
+//@module
+
+//All Categories
+
+var categories = [];
+
+
+
+//Category objects should have a name, color, its subcategories, and the clothing within its category
+//
+//Example usage:
+//
+//		ASSUME: baseball, basketball, soccer are Category objects
+//  	ASSUME: blueShirt, whitePants are Clothing objects
+//
+//		subcatoriesArray = [baseball, basketball, soccer];
+//  	clothingArray = [blueShirt, whitePants]
+//		sports = category.Category(name="Sports", color="orange", subcategories=subcategoriesArray, clothing=clothingArray);
+
+var Category = function(name, color, subcategories, clothing) {
+	var returnCategory = Object();
+	returnCategory.name = name;
+	returnCategory.color = color;
+	returnCategory.subcategories = subcategories;
+	returnCategory.clothing = clothing;
+	return returnCategory;
+}
+
+exports.Category = Category;
+exports.categories = categories;
