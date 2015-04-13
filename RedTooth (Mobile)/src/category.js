a65f35d77a2c2e956ea9acfbc646dee1b0d7ed3b
Linda <linda.he@berkeley.edu>
2015-04-13 00:44:17 -0700

    you can now add categories when adding clothing; this then adds the clothing to each of the categories

--- RedTooth (Mobile)/src/category.js
+++ RedTooth (Mobile)/src/category.js
@@ -15,9 +15,9 @@ var categorySkinColors = [limeGreenVariantColor, watermelonVariantColor, cremeBr
 var categories = [];
 
 var sampleCategories = [
-	    {name: 'Business', color:"orange"},
-    	{name: 'Date', color:"blue"},
-    	{name: 'Casual', color:"white"},
+	    {name: 'Business', color: renaissanceBlueVariantColor, clothing: []},
+    	{name: 'Date', color: watermelonVariantColor, clothing: []},
+    	{name: 'Casual', color: orangeCreamsicleVariantColor, clothing: []},
 	];
 
 var Category = function(name, color, subcategories, clothing) {
@@ -30,6 +30,17 @@ var Category = function(name, color, subcategories, clothing) {
 	return returnCategory;
 };
 
+function addClothingToCategories(selectedCategoriesArray, clothingObject) {
+	for (var i = 0; i < selectedCategoriesArray.length; i++) {
+		for (var j = 0; j < categories.length; j++) {
+			if (selectedCategoriesArray[i].name == categories[j].name) {
+				categories[j].clothing.push(clothingObject);
+				trace(clothingObject.name + " is being added to " + categories[j].name + "\n");
+				trace("TRACE ME");
+			}
+		}
+	}
+}
 
 var generateCategorySkinColor = function() {
 	return categorySkinColors[categories.length % 8];
@@ -42,9 +53,11 @@ for (var i = 0; i < sampleCategories.length; i++) {
 	var newCategory = new Category();
 	newCategory.name = sampleCategories[i].name;
 	newCategory.color = sampleCategories[i].color;
+	newCategory.clothing = sampleCategories[i].clothing;
 	categories.push(newCategory);
 }
 
 exports.Category = Category;
 exports.categories = categories;
 exports.generateCategorySkinColor = generateCategorySkinColor;
+exports.addClothingToCategories = addClothingToCategories;
