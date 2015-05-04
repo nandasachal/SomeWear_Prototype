c6619a56c529b7ba756cf79042949f3e67650922
Annie Lin <annielin195@gmail.com>
2015-05-03 19:44:04 -0700

    Updated clothing profile

---
+++ RedTooth (Mobile)/src/clothingProfileNavBar.js
@@ -0,0 +1,86 @@
+// KPR Script file
+var BUTTONS = require('controls/buttons');
+
+var bigText = new Style({font:"bold 30px", color:"black"});
+var clothingProfile = require('clothingProfile.js');
+var lightManager = require('lightManager.js');
+var clothing = require('clothing.js');
+
+var tealVariantSkin = new Skin({fill:'#FF52b0b0'});
+var headerStyle = new Style({font: 'Roboto bold 50px', color: 'white', align: "center,right"});
+var buttonStyle = new Style({font: 'Roboto bold 50px', color: 'white', align:'middle'});
+var briefcaseTexture = new Texture('../assets/switchToCategoriesGraphic.png');
+//var clothingTexture = new Texture('../assets/tShirtMockNEW.png');
+var clothingTexture = new Texture('../assets/switchToClothingGraphic.png');
+var addTexture = new Texture('../assets/okayButtonGraphic.png');
+var clothingIconSkin = new Skin({ texture: clothingTexture, height: 55, width:70, aspect: 'fit', });
+var briefcaseIconSkin = new Skin({ texture: briefcaseTexture, height: 55, width: 70, aspect: 'fit', });
+var addButtonSkin = new Skin({ texture: addTexture, top: 10, height:55, width: 70, aspect: 'fit', });
+
+
+//common colors
+var darkerTealColorLightOpacity = "#bb144644";
+var darkerTealColorHeavy = "#FF347A75";
+var tealColor = "#FF52b0b0";
+var lighterTealColor = "#ff84D3D1";
+var lightestTealColor = "#ffDEFCFA";
+
+
+var tealSkin = new Skin({fill: lightestTealColor});
+var transparentTealSkin = new Skin({fill: darkerTealColorLightOpacity});
+
+var editText = new Text({left:20, right:10, top: 10, height: 40, string: "This is the edit button. It's still a work in progress", style: bigText}),
+var editCon = new Container({ top: 90, right: 30, bottom: 90, left: 30, skin: transparentTealSkin, contents:[editText],});
+
+
+
+var buttonTemplate = BUTTONS.Button.template(function($, name){ return{
+	top:0, bottom:0, left:0, right:0, height: 50, width: 10,
+	contents:[
+		new Label({left:0, right:0, top:0, bottom:0, string:$.textForLabel, style:buttonStyle})
+	],
+	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+		onTap: { value: function(content) {
+			if (content == backButton) {
+				application.remove(navBar);
+				application.remove(clothingProfile.newCon);
+				lightManager.dimAll();
+			}
+			
+			if (content == deleteIcon) {
+				clothingProfile.remove();
+			}
+			if (content == editIcon) {
+				//bring up edit page
+				application.add(editCon);
+			}
+		}}
+	}),
+	name: name
+}});
+
+//UI Elements
+ 
+var backButton = new buttonTemplate({top: 5, textForLabel:'', name: 'backButton'});
+backButton.skin = addButtonSkin;
+
+var deleteIcon = new buttonTemplate({top: 5, textForLabel:'', name: 'deleteIcon'});
+deleteIcon.skin = briefcaseIconSkin;
+
+var editIcon = new buttonTemplate({top: 5, textForLabel:'', name: 'editIcon'});
+editIcon.skin = clothingIconSkin;
+
+
+var navBar = new Line({left:0, right:0, top:0, bottom:420, height: 50, skin: tealVariantSkin, name: 'titleBar', contents:[
+	//switchIcon,
+	//new Label({left:30, right:0, top:0, bottom:0, height: 30, width: 40, name:"titleWords", string:"", style:headerStyle}),
+	backButton,
+	deleteIcon,
+	editIcon,
+	]
+});
+
+
+//External Items
+
+exports.navBar = navBar;
